f01cf6acf3ae6dc84b8cf55a94e458f2f918fec2
Eduardo Granados <eduardo212792@gmail.com>
2019-05-24 16:27:46 -0600

    Clase que abre ventanta de inicio con menu
    *Falta agregar aun los comandos a los botones

--- mainScreen.py
+++ mainScreen.py
@@ -1,20 +1,50 @@
+
 from tkinter import *
+"""
+Clase que dibuja la ventana principal
+"""
+class Ventana:
+
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
 
 
 
-#Set Up de Ventana
-width = 800
-height = 400
-#Ventana Principal
+    def draw(self):
+        #Ventana Principal
+        V_inicio = Tk()
+        V_inicio.title('Formula CE')
+        V_inicio.minsize(self.width, self.height)
+        V_inicio.resizable(width=NO, height=NO)
+        #_________________________
+        # Canvas de la ventana...
+        #_________________________
 
-V_inicio=Tk()
-V_inicio.title('Formula CE')
-V_inicio.minsize(width, height)
-V_inicio.resizable(width=NO, height=NO)
+        C_inicio = Canvas(V_inicio, width=self.width, height=self.height, bg='white')
+        C_inicio.place(x=0, y=0)
 
-# Canvas de la ventana...
-C_inicio=Canvas(V_inicio, width=800, height=600, bg='white')
-C_inicio.place(x=0, y=0)
+        #_______________________________________
+        # Crea la cinta de opciones del programa
+        #_______________________________________
+        cinta_opciones = Menu(V_inicio)
+        V_inicio.config(menu=cinta_opciones)
+
+        # Boton Home
+        cinta_opciones.add_cascade(label='Home')
+        cinta_opciones.add_separator()
+
+        # Boton about
+        cinta_opciones.add_cascade(label='About')
+        cinta_opciones.add_separator()
+
+        # Boton posiciones
+        cinta_opciones.add_cascade(label='Positions')
+        cinta_opciones.add_separator()
+
+        # boton de Test drive
+        cinta_opciones.add_cascade(label='Test drive')
 
 
-V_inicio.mainloop()
+        V_inicio.mainloop()
+
