baead4f47e60e8e14793296794ba6fc0df1b8818
Eduardo Granados <eduardo212792@gmail.com>
2019-05-24 18:37:51 -0600

    Documentacion de Clase Ventana

--- mainScreen.py
+++ mainScreen.py
@@ -2,43 +2,77 @@
 from tkinter import *
 """
 Clase que dibuja la ventana principal
+
+Atributos:
+height(entrada)>>> int, valor del alto de la ventana a crear
+width(entrada)>>> int, valor del ancho de la ventana a crear
+
+V_inicio >>> atributo que tiene como valor una instancia de ventana en tkinter
+V_inicio.title >>> nombre de la ventana (titulo)
+V_inicio >>> medidas de la ventana
+V_inicio.resizable >>> control sobre el cambio de tamano de la ventana
+
+C_inicio >>> canvas principal
+
+cinta_opciones >>> menu en la ventana
+V_inicio.config >>> configuracion de la ventana, para el menu.
+
+Metodos:
+
+canvas_clean >>> funcion que resetea el canvas, para hacer el cambio de pantalla
+test_drive >>> funcion que dibuja todo lo necesario para la pantalla de pruebas
+positions >>> dibuja la ventana de posiciones
+about >>> dibuja la ventana de informacion
+inicio >>> dibuja la ventana principal
+
+__draw__ >>> funcion que pone todo en la pantalla y la actualiza
+
 """
+
+
 class Ventana:
 
-    def __init__(self, height, width):
+    def __init__(self, width, height):
+
+        #-------------------Settings of window-----------------#
         self.height = height
         self.width = width
-        self.flag = True
-        #Settings of window
 
         #Window itself
         self.V_inicio = Tk()
         self.V_inicio.title('Formula CE')
         self.V_inicio.minsize(self.width, self.height)
         self.V_inicio.resizable(width=NO, height=NO)
+
         #Main canvas
         self.C_inicio = Canvas(self.V_inicio, width=self.width, height=self.height, bg='white')
+        self.C_inicio.place(x=0, y=0)
 
         #Main menu
-
         self.cinta_opciones = Menu(self.V_inicio)
         self.V_inicio.config(menu=self.cinta_opciones)
 
-    def Quit(self):
-        self.flag = False
-        print('quit')
     def canvas_clean(self):
-        pass
+        self.C_inicio.delete('all')
+
     def test_drive(self):
-        
+        #lineas guias
+        #______________________________
+        self.C_inicio.create_line(0,(self.height/2),self.width,(self.height/2)) #horizontal
 
+        self.C_inicio.create_line((self.width/2),0,  (self.width /2), self.height,) #vertical
 
+    def inicio(self):
+        pass
+
+    def about(self):
+        pass
+
+    def positions(self):
+        pass
 
     def __draw__(self):
-        #_________________________
-        # Canvas de la ventana...
-        #_________________________
-        self.C_inicio.place(x=0, y=0)
+
         # Boton Home
         self.cinta_opciones.add_cascade(label='Home')
         self.cinta_opciones.add_separator()
@@ -52,12 +86,11 @@ class Ventana:
         self.cinta_opciones.add_separator()
 
         # boton de Test drive
-        self.cinta_opciones.add_cascade(label='Test drive')
+        self.cinta_opciones.add_cascade(label='Test drive',command= self.test_drive)
 
-        # boton de salida
-        self.cinta_opciones.add_cascade(label='Quit', command=self.Quit)
+
         self.V_inicio.mainloop()
 
-ventana = Ventana(500,500)
+ventana = Ventana(1024,728)
 
 ventana.__draw__()
