12d8b559f7ff5e1b9228c9d647eef6b7f02653a2
Eduardo Granados <eduardo212792@gmail.com>
2019-06-06 03:20:13 -0600

    FINALMENTE,  comandos funcionales
    Falta la prueba nada mas

--- mainScreen.py
+++ mainScreen.py
@@ -29,14 +29,7 @@ cinta_opciones >>> menu en la ventana
 V_inicio.config >>> configuracion de la ventana, para el menu.
 
 Metodos:
-
-canvas_clean >>> funcion que resetea el canvas, para hacer el cambio de pantalla
-test_drive >>> funcion que dibuja todo lo necesario para la pantalla de pruebas
-positions >>> dibuja la ventana de posiciones
-about >>> dibuja la ventana de informacion
-inicio >>> dibuja la ventana principal
-
-__draw__ >>> funcion que pone todo en la pantalla y la actualiza
+__draw__ >>> funcion que dibuja todo lo necesario para la pantalla de pruebas
 
 """
 myCar = NodeMCU()
@@ -58,93 +51,120 @@ def get_log():
             indice += 1
         time.sleep(0.200)
 
+
 p = Thread(target=get_log)
-p.start()
+
 
 
 class Test_Drive:
 
-    def __init__(self, width, height):
+    def __init__(self):
 
         #-------------------Settings of window-----------------#
-        self.height = height
-        self.width = width
+        self.height = 728
+        self.width = 1024
 
         #Window itself
-        self.V_inicio = Tk()
-        self.V_inicio.title('Formula CE')
-        self.V_inicio.minsize(self.width, self.height)
-        self.V_inicio.resizable(width=NO, height=NO)
+        self.V_test = Tk()
+        self.V_test.title('Formula CE')
+        self.V_test.minsize(self.width, self.height)
+        self.V_test.resizable(width=NO, height=NO)
 
         #Main canvas
-        self.C_inicio = Canvas(self.V_inicio, width=self.width, height=self.height, bg='#f0f0f0')
-        self.C_inicio.place(x=0, y=0)
+        self.C_test = Canvas(self.V_test, width=self.width, height=self.height, bg='#f0f0f0')
+        self.C_test.place(x=0, y=0)
 
-        #Main menu
-        self.cinta_opciones = Menu(self.V_inicio)
-        self.V_inicio.config(menu=self.cinta_opciones)
-
+        #Configuracion del carro, variables necesarias
         self.pwm = 0
         self.dir_state = 0
+        self.lf = 0
+        self.lb = 0
+        self.ld = 0
 
     def cargarImagen(self, nombre):
         ruta = os.path.join('resources', nombre)
         imagen = PhotoImage(file=ruta)
         return imagen
 
-    def test_drive(self, car, driver, country, team):
-
+    def __draw__(self, car, driver, country, team):
+        aceleracion = 100
         count = StringVar()
         count.set(0)
 
         battery_level = StringVar()
         battery_level.set(100)
         color = 'black'
+
         dir_state = 0 #variable que guarda estado de direccionales
 
         # <--- | ^  |  --->
         #  -1  | 0  |  1
-        def acelera(event):
+
+        #YA ENVIAA
+        def acelera(event): #YA ENVIA COMANDOS
 
             if self.pwm == 1000:
                 print("velocidad maxima")
             else:
-                self.pwm += 10
+                self.pwm += aceleracion
                 count.set(self.pwm)
+                msg = 'pwm: ' + str(self.pwm)+ ' ;'
+                myCar.send(msg)
                 cambiaColor()
-        def reverse(event):
+                print('comando enviado: ', msg)
+        #YA ENVIA
+        def reverse(event): #YA ENVIA COMANDOS
 
             if self.pwm == -600:
                 print("reversa maxima")
             else:
 
-                self.pwm-=10
+                self.pwm-=aceleracion
                 count.set(self.pwm)
+                msg = 'pwm: ' + str(self.pwm)+ ' ;'
+                myCar.send(msg)
+                print('comando enviado: ', msg)
                 cambiaColor()
-        def dir_lights_right():
+        #YA ENIVIA
+        def dir_lights_right(): #YA ENVIA COMANDOS
 
             if self.dir_state == -1:
                 self.dir_state = 0
-                print('direccional derecha: ', self.dir_state)
+                msg = 'lr: 0;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
+
+
             else:
+
                 self.dir_state = 1
-                print('direccional derecha: ', self.dir_state)
+                msg = 'lr:1;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
                 time.sleep(0.5)
 
                 self.dir_state = 0
-                print('direccional derecha: ', self.dir_state)
+                msg = 'lr:0;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
                 time.sleep(0.5)
 
                 self.dir_state = 1
-                print('direccional derecha: ', self.dir_state)
+                msg = 'lr:1;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
                 time.sleep(0.5)
 
                 self.dir_state = 0
-                print('direccional derecha: ', self.dir_state)
+                msg = 'lr:0;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
+
                 time.sleep(0.5)
 
                 print('done')
-        def dir_lights_left():
+        #YA ENVIA
+        def dir_lights_left(): #YA ENVIA COMANDOS
 
             if self.dir_state == 1:
                 self.dir_state = 0
@@ -153,76 +173,142 @@ class Test_Drive:
             else:
 
                 self.dir_state = -1
-                print('direccional izquierda: ', self.dir_state)
+                msg = 'll:1;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
                 time.sleep(0.5)
 
                 self.dir_state = 0
-                print('direccional izquierda: ', self.dir_state)
+                msg = 'll:0;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
                 time.sleep(0.5)
 
                 self.dir_state = -1
-                print('direccional izquierda: ', self.dir_state)
+                msg = 'll:1;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
                 time.sleep(0.5)
 
                 self.dir_state = 0
-                print('direccional izquierda: ', self.dir_state)
+                msg = 'll:0;'
+                myCar.send(msg)
+                print('Comando enviado: ', msg)
+
+
+
                 time.sleep(0.5)
 
 
                 print('done')
+        #YA ENVIA
         def gira_derecha():
             turn = 1
-            print('dir: ', turn)
+            msg = 'dir:1;'
+            myCar.send(msg)
+            print('Comando giro enviado: ',msg)
+
             dir_lights_right()
+            msg = 'dir:0;'
+            myCar.send(msg)
+            print('Comando giro enviado: ', msg)
+
             turn = 0
-            print('dir: ', turn)
+        #YA ENVIA
         def gira_izquierda():
 
             turn = -1
-            print('dir: ' ,turn)
+            msg = 'dir:-1;'
+            myCar.send(msg)
+            print('Comando giro enviado: ', msg)
+
             dir_lights_left()
+            msg = 'dir:0;'
+            myCar.send(msg)
+            print('Comando giro enviado: ', msg)
+
             turn = 0
-            print("dir: " , turn )
+        #YA ENVIA
         def brake(event):
             if self.pwm > 0:
-                self.pwm -= 10
+                self.pwm -= 50
                 count.set(self.pwm)
+                msg = 'pwm: ' + str(self.pwm)+ ' ;'
+                myCar.send(msg)
+                print('comando enviado: ', msg)
                 cambiaColor()
             elif self.pwm <0:
-                self.pwm +=10
+                self.pwm +=50
                 count.set(self.pwm)
+                msg = 'pwm: ' + str(self.pwm)+ ' ;'
+                myCar.send(msg)
+                print('comando enviado: ', msg)
                 cambiaColor()
+        #====================FALTA DEFINIR=============#
         def movimiento_especial(event):
             print("Envia movimiento especial...")
+        #====================FALTA DEFINIR=============#
         def celebracion(event):
             print("Envia celebracion...")
+
+        #YA ENVIA
         def enciende_frontales(event):
-            print('enciende frontales')
+            if self.lf == 0:
+                myCar.send('lf:1;')
+                print('Comando enviado: ', 'lf:1;')
+                self.lf = 1
+            else:
+                myCar.send('lf:0;')
+                print('Comando enviado: ', 'lf:0;')
+                self.lf = 0
+        #YA ENVIA
         def enciende_traseras(event):
-            print('eciende traseras')
+            if self.lb == 0:
+                myCar.send('lb:1;')
+                print('Comando enviado: ', 'lb:1;')
+                self.lb = 1
+            else:
+                myCar.send('lb:0;')
+                print('Comando enviado: ', 'lb:0;')
+                self.lb = 0
+        #YA ENVIA
         def enciende_emergencia(event):
-            print("enciende ambas direccionales")
+            if self.ld == 0:
+                myCar.send('ll:1;')
+                myCar.send('lr:1;')
+                print('Comando enviado: ', 'lf:1;', 'lr:1;')
+                self.ld = 1
+            else:
+                myCar.send('ll:0;')
+                myCar.send('lr:0;')
+                print('Comando enviado: ', 'll:0;', 'lr:0;')
+                self.ld = 0
+        #YA ENVIA
         def enciende_all_lights(event):
             enciende_frontales(event)
             enciende_traseras(event)
             enciende_emergencia(event)
             print('enciende todas las luces.....')
 
+        def read_battery():
+
+            level = myCar.readById('blvl')
+            battery_level.set(level)
+            print('Bateria actualizada..')
 
         #Etiquetas de informacion, carro, piloto...
-        #Etiqueta del modelo del carro...
-        marcaAuto = Label(self.C_inicio, text= 'Model: ' + str(car), font=("Arial ", 20), justify=CENTER)
+        #Etiqueta del modelo del carro.
+        marcaAuto = Label(self.C_test, text='Model: ' + str(car), font=("Arial ", 20), justify=CENTER)
         marcaAuto.place(x=0,y=10)
-
-        Nombre = Label(self.C_inicio, text = "Driver: " +str(driver), font=("Arial", 20), width = 15,justify= LEFT)
+        #Etiqueta conductor.
+        Nombre = Label(self.C_test, text ="Driver: " + str(driver), font=("Arial", 20), width = 15, justify= LEFT)
         Nombre.place(x=-25, y= 50)
-
-        Country = Label(self.C_inicio, text= str(country),font=('Arial', 20), justify=CENTER)
+        #Etiqueta pais
+        Country = Label(self.C_test, text= str(country), font=('Arial', 20), justify=CENTER)
         Country.place(x=220, y= 50)
-
-        Team = Label(self.C_inicio, text = 'Team: ' + str(team),font=('Arial', 20), justify=CENTER )
+        #Etiqueta del equipo
+        Team = Label(self.C_test, text ='Team: ' + str(team), font=('Arial', 20), justify=CENTER)
         Team.place(x=0, y =90)
-
 
 
         #---|------------------------|
@@ -231,61 +317,24 @@ class Test_Drive:
 
         #---------------Volante con medidor velocidad----------------#
         steer_image = self.cargarImagen('bg.png')
-        steer = Label(self.C_inicio, image = steer_image)
+        steer = Label(self.C_test, image = steer_image)
         steer.image = steer_image
         steer.place(x=0,y=450)
 
+
         #-----------------Indicador velocidad------------------------#
-        speed = Label(self.C_inicio, textvariable=count,font=('Unispace', 45),fg=color)
+        speed = Label(self.C_test, textvariable=count, font=('Unispace', 45), fg=color)
         speed.place(x=490,y=570)
 
-        #Funcion que cambia el color del indicador
-        def cambiaColor():
-
-            if self.pwm == 0:
-                speed.config(fg = 'green')
-
-            elif self.pwm >= 10 and self.pwm <= 490:
-                speed.config(fg= 'blue')
-
-            elif self.pwm >= 500:
-                speed.config(fg='red')
-
-            elif self.pwm <= -10 and self.pwm >= -490:
-                speed.config(fg = 'black')
-            elif self.pwm <=-500:
-                speed.config(fg='red')
-
         #--------------------Indicador de bateria--------------------#
-        mark_battery = Label(self.C_inicio, text=' Battery Level: ',font=('Arial', 20))
-        bat_level = Label(self.C_inicio, textvariable= (battery_level), font=('Unispace', 20))
-        bat_level.place(x=950,y=0)
-        mark_battery.place(x=700,y=0)
-        #-----------------Threads necesarios--------------------------#
-
-        #Threads para encender y apagar direccionales:
-
-        def thread_dir_izq(event):
-            dir_izq_thread = Thread(target= dir_lights_left, args=())
-            dir_izq_thread.start()
-        def thread_dir_der(event):
-
-            dir_der_thread = Thread(target=dir_lights_right, args=())
-            dir_der_thread.start()
-
-        #Threads para girar, y encender las direccionales
-        def thread_turn_right(event):
-            turn_right = Thread(target=gira_derecha, args=())
-            turn_right.start()
-        def thread_turn_left(event):
-            turn_left = Thread(target = gira_izquierda,args=())
-            turn_left.start()
+        mark_battery = Label(self.C_test, text=' Battery Level: ', font=('Arial', 20))
+        bat_level = Label(self.C_test, textvariable= (battery_level), font=('Unispace', 20))
+        bat_level.place(x=940,y=0)
+        mark_battery.place(x=750,y=0)
 
         #------------------Boton ayuda--------------------------------#
-
-
         def ayuda():
-            Ayuda = Canvas(self.V_inicio, width = 400, height=600, bg='white')
+            Ayuda = Canvas(self.V_test, width = 400, height=600, bg='white')
             Ayuda.place(x=300, y= 50)
 
             Ayuda.create_text(200,20,text = 'How to drive ?',font=('Unispace', 20))
@@ -307,44 +356,65 @@ class Test_Drive:
                 Ayuda.destroy()
             exit_button = Button(Ayuda, bitmap = 'error', command=exit)
             exit_button.place(x=380,y=580)
+        botton_help = Button(self.C_test, bitmap='question', relief = FLAT, command= ayuda)
+        botton_help.place(x=990, y=695)
+        #--------------------Boton Update bateria---------------------#
+        refresh = Button(self.C_test, bitmap ='warning', command= read_battery)
+        refresh.place(x=1010,y=5)
+        #Funcion que cambia el color del indicador
+        def cambiaColor():
 
+            if self.pwm == 0:
+                speed.config(fg = 'green')
 
+            elif self.pwm >= 10 and self.pwm <= 490:
+                speed.config(fg= 'blue')
 
+            elif self.pwm >= 500:
+                speed.config(fg='red')
 
+            elif self.pwm <= -10 and self.pwm >= -490:
+                speed.config(fg = 'black')
+            elif self.pwm <=-500:
+                speed.config(fg='red')
 
-        botton_help = Button(self.C_inicio, bitmap= 'question', relief = FLAT, command= ayuda)
-        botton_help.place(x=1000, y=695)
+        #-----------------Threads necesarios--------------------------#
+        #Threads para encender y apagar direccionales:
+        def thread_dir_izq(event):
+            dir_izq_thread = Thread(target= dir_lights_left, args=())
+            dir_izq_thread.start()
+        def thread_dir_der(event):
+
+            dir_der_thread = Thread(target=dir_lights_right, args=())
+            dir_der_thread.start()
+        #Threads para girar, y encender las direccionales
+        def thread_turn_right(event):
+            turn_right = Thread(target=gira_derecha, args=())
+            turn_right.start()
+        def thread_turn_left(event):
+            turn_left = Thread(target = gira_izquierda,args=())
+            turn_left.start()
+
         #---------------------------Binding Events--------------------#
-        self.V_inicio.bind("w",acelera) ##Acelerador, con tecla W
-        self.V_inicio.bind("s",  reverse) ##Freno, con tecla S
-        self.V_inicio.bind("<Right>", thread_turn_right)
-        self.V_inicio.bind("<Left>",thread_turn_left)
-        self.V_inicio.bind("<space>", brake)
-        self.V_inicio.bind("<Shift-Right>",dir_lights_right)
-        self.V_inicio.bind("<Shift-Left>", thread_dir_izq)
-        self.V_inicio.bind("l", enciende_frontales)
-        self.V_inicio.bind('b', enciende_traseras)
-        self.V_inicio.bind('e', enciende_emergencia)
-        self.V_inicio.bind("p", enciende_all_lights)
-        self.V_inicio.bind('z',movimiento_especial)
-        self.V_inicio.bind('c', celebracion)
-
-    def __draw__(self):
-
-        # Boton Home
-        self.cinta_opciones.add_cascade(label='Home')
-        self.cinta_opciones.add_separator()
-        # Boton about
-        self.cinta_opciones.add_cascade(label='About')
-        self.cinta_opciones.add_separator()
-        # Boton posiciones
-        self.cinta_opciones.add_cascade(label='Positions')
-        self.cinta_opciones.add_separator()
-        # boton de Test drive
-        self.cinta_opciones.add_cascade(label='Test drive',command= lambda: self.test_drive('Ferrari 428 italia ', 'Eduardo', 'CRC','Redbull'))
-        self.V_inicio.mainloop()
+        self.V_test.bind("w", acelera) ##Acelerador, con tecla W
+        self.V_test.bind("s", reverse) ##Freno, con tecla S
+        self.V_test.bind("<Right>", thread_turn_right)
+        self.V_test.bind("<Left>", thread_turn_left)
+        self.V_test.bind("<space>", brake)
+        self.V_test.bind("<Shift-Right>", dir_lights_right)
+        self.V_test.bind("<Shift-Left>", thread_dir_izq)
+        self.V_test.bind("l", enciende_frontales)
+        self.V_test.bind('b', enciende_traseras)
+        self.V_test.bind('e', enciende_emergencia)
+        self.V_test.bind("a", enciende_all_lights)
+        self.V_test.bind('z', movimiento_especial)
+        self.V_test.bind('c', celebracion)
 
 
-ventana = Test_Drive(1024, 728)
-ventana.__draw__()
+        self.V_test.mainloop()
+
+
+
+ventana = Test_Drive()
+ventana.__draw__('Ferrari 458', 'Eduardo', 'CRC', 'Red Bull')
 
